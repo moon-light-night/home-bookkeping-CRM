d4853b5d77d07096b817fad2775cc9c7a2b9d464
Ilya Baskakov <baskakov.11111@mail.ru>
2021-02-03 13:56:36 +0300

    created links in sidebar

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,26 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-// import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-  // {
-  //   path: "/",
-  //   name: "Home",
-  //   component: Home
-  // },
+  {
+    path: "/",
+    name: "Home",
+    meta: { layout: "main" },
+    component: () => import("../views/Home.vue")
+  },
   {
     path: "/login",
     name: "Login",
     meta: { layout: "empty" },
     component: () => import("../views/Login.vue")
+  },
+  {
+    path: "/register",
+    name: "Register",
+    meta: { layout: "empty" },
+    component: () => import("../views/Register.vue")
   },
   {
     path: "/categories",
@@ -33,12 +39,6 @@ const routes = [
     name: "History",
     meta: { layout: "main" },
     component: () => import("../views/History.vue")
-  },
-  {
-    path: "/home",
-    name: "Home",
-    meta: { layout: "main" },
-    component: () => import("../views/Home.vue")
   },
   {
     path: "/planning",
